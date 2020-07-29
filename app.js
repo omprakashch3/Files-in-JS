674d4493c11076fca08a90004b00987bd4c82707
omprakashch3 <omprakashchautala3@gmail.com>
2020-07-29 20:33:46 +0530

    basic reading

---
+++ app.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+var greet = fs.readFileSync(__dirname + "/greet.txt", "utf8");
+
+console.log(greet);
+
+var greet2 = fs.readFile(__dirname + "/greet.txt", "utf8", function (
+  err,
+  data
+) {
+  console.log(data);
+});
+
+console.log(greet2);
+
+console.log("done");
